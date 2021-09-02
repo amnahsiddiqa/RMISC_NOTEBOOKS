5f8f376a9a42b9d3a0a85d19547aac11a23e7734
Amnah Siddiqa <amnahsiddiqa@gmail.com>
2021-09-02 10:11:41 -0400

    Smallupdates

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,18 @@
 
+## Random snippets for my short term memory loss
+## exporting nested list 
+
+import csv
+
+data = [[74.9818, 0.09],
+ [75.0021, 0.18],
+ [75.0022, 0.17],
+ [75.0026, 0.07],
+ [75.0027, 0.07],]
+
+
+pd.DataFrame(data).to_csv("output.csv",index=False)
+
+
+## printing nested list
+print(statistics[:3])  
